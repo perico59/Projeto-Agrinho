88936015808aeb2efce3a3ef31969b296d5d985c
perico59 <157078574+perico59@users.noreply.github.com>
2024-08-19 19:32:53 -0300

    Add files via upload

---
+++ Agrinho-CSS/script.js
@@ -0,0 +1,7 @@
+class MobileNavbar {
+    constructor(mobileMenu, menuButton, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.menuButton = document.querySelector(menuButton);
+        this.navLinks = document.querySelector(navLinks);
+    }
+}
